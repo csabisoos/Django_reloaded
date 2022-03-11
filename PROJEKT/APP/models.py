42dbef91ba8c30f0546d506dfba041794251bce4
Csaba Soós <csabi.soos.05@gmail.com>
2022-03-11 13:06:32 +0100

    oldal létrehozása + kerdesek

--- PROJEKT/APP/models.py
+++ PROJEKT/APP/models.py
@@ -3,15 +3,23 @@ from django.db import models
 # Create your models here.
 
 
+
 class Kerdes(models.Model):
     class Meta:
         verbose_name = 'Kérdés'
         verbose_name_plural = 'Kérdések'
     
     kerdes = models.CharField(max_length=255)
-    helyesvalasz = kerdes = models.CharField(max_length=255)
-    rosszvalasz1 = kerdes = models.CharField(max_length=255)
-    rosszvalasz2 = kerdes = models.CharField(max_length=255)
+    helyesvalasz = models.CharField(max_length=255)
+    rosszvalasz1 = models.CharField(max_length=255)
+    rosszvalasz2 = models.CharField(max_length=255)
+    rosszvalasz3 = models.CharField(max_length=255)
+    szam = models.IntegerField()
+    logikai = models.BooleanField()
+    valosszam = models.FloatField()
+    hosszuszoveg = models.TextField()
+    email = models.EmailField()
+    url = models.URLField()
 
     def __str__(self): #ToString ekvivalense pythonban
-        pass
+        return f"{self.kerdes}: {self.helyesvalasz}"
--- PROJEKT/APP/views.py
+++ PROJEKT/APP/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from random import randint, random
+from .models import Kerdes # a mostani konyvtarban levo models
 
 # Create your views here.
+
+def bigyoview (request):
+    kerdesek = Kerdes.objects.all()
+
+    for kerdes in kerdesek:
+        print(kerdes)
+
+    template = "bigyotemplate.html"
+    context = {'a' : randint(0, 10), 'kerdesek2': kerdesek}
+    return render(request, template, context)
